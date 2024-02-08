ecc17b4d3820c6958f3a94bc395952ac90278e93
Thierry <130665360+Wiped-outt@users.noreply.github.com>
2024-02-08 12:21:48 -0200

    a

--- 2_Cadastro (Alunos_)/visualizar.php
+++ 2_Cadastro (Alunos_)/visualizar.php
@@ -1,2 +1,38 @@
-<!-- Digite aqui (visualizar.php) -->
-<!-- 2º Arquivo a ser digitado -->
+<?php
+
+// Incluir a conexao com o banco de dados
+include_once "conexao.php";
+
+// Receber o ID via método GET
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+//  Verifica o ID não está vazio
+if (!empty($id)) {
+    // Query para selecionar informações de usuário e endereço associado
+    $query_usuario = "SELECT usr.id, usr.nome, usr.email,
+            ende.logradouro, ende.numero
+            FROM usuarios AS usr
+            LEFT JOIN enderecos AS ende ON ende.usuario_id=usr.id
+            WHERE usr.id=:id LIMIT 1";
+    $result_usuario = $conn->prepare($query_usuario);
+    $result_usuario->bindParam(':id', $id);
+    $result_usuario->execute();
+
+    // Verificar se a consulta foi bem-sucedida e se há registros
+    if (($result_usuario) and ($result_usuario->rowCount() != 0)) {
+        // Recupera os dados do usuário e endereço associado
+        $row_usuario = $result_usuario->fetch(PDO::FETCH_ASSOC);
+        $retorna = ['status' => true, 'dados' => $row_usuario];
+    } else {
+        // Se não houver registros, retorja uma mensgadem de erro
+        $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Nenhum
+        usuário encontrado!</div>"];
+    }
+} else {
+    // Se o ID estiver vazio, retorna uma mensagem de erro
+    $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Nenhum
+    usuário encontrado!</div>"];
+}
+
+// Retorna o resultado em formato JSON
+echo json_encode($retorna);
